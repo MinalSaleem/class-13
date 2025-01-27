fe8f320bf1d54001dd1ba6733b7b798c5398a24a
MinalSaleem <minalsaleem9@gmail.com>
2025-01-28 00:02:23 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,8 @@ export default function Home() {
       </div>
 
       <div className="flex-grow container mx-auto px-4 py-10 text-center">
-        <h1 className="text-5xl font-extrabold text-slate-900  mb-6">
+
+        <h1 className="text-5xl font-extrabold text-slate-900 mb-6">
           Welcome to Clerk
         </h1>
         <p className="text-lg text-slate-800 mb-8">
@@ -29,12 +30,13 @@ export default function Home() {
         <button className="bg-slate-900  text-white px-6 py-3 rounded shadow-md hover:bg-white hover:text-slate-800 focus:outline-none focus:ring-2 focus:ring-slate-700 hover:border-slate-900">
           Learn More
         </button>
+
       </div>
 
       <div className="w-full bg-slate-900  text-white p-4 text-center">
         <p className="text-sm">© 2024 My App. All rights reserved.</p>
       </div>
-      
+
     </div>
   );
 }
